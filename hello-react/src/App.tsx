2953bf07697db359fa91c64fda1ea21f0ef577ed
ARAVAPALLY SHIVARAMAKRISHNA <srkarvapally@gmail.com>
2023-08-06 00:19:39 +0530

    changes according to requirement of rejection

--- hello-react/src/App.tsx
+++ hello-react/src/App.tsx
@@ -1,3 +1,4 @@
+import React from "react"
 import TaskCard from "./TaskCard"
 import './TaskCard.css'
 
@@ -6,14 +7,14 @@ function App() {
     <div>
       <div>
         <h1 className="text-xl underline py-1 font-bold text-gray-500">Pending Tasks:</h1>
-        <TaskCard title="Build the website with static content" dueDate="10th August" Assignee="Shivaramakrishna"/>
-        <TaskCard title="Review-1" dueDate="5th September" Assignee="Satya"/>
+        <TaskCard title="Build the website with static content" dueDate="10th August" assigneeName="Shivaramakrishna"/>
+        <TaskCard title="Review-1" dueDate="5th September" assigneeName="Satya"/>
       </div>
       <div>
         <h1 className="text-xl underline py-1 font-bold text-gray-500">Done Tasks:</h1>
-        <TaskCard title="Idea and Abstract" comDate="14th June" Assignee="Sathvik"/>
-        <TaskCard title="Designed Mockup" comDate="21th July" Assignee="Amit"/>
-        <TaskCard title="Structure and Associations" comDate="29th July" Assignee="Paul"/>
+        <TaskCard title="Idea and Abstract" completedAtDate="14th June" assigneeName="Sathvik"/>
+        <TaskCard title="Designed Mockup" completedAtDate="21th July" assigneeName="Amit"/>
+        <TaskCard title="Structure and Associations" completedAtDate="29th July" assigneeName="Paul"/>
       </div>
     </div>
   )
--- hello-react/src/TaskCard.tsx
+++ hello-react/src/TaskCard.tsx
@@ -3,13 +3,13 @@ const TaskCard = (props) => {
     let date = "Due on: "+props.dueDate;
     if(!props.dueDate)
     {
-        date = "Completed on: "+props.comDate;
+        date = "Completed on: "+props.completedAtDate;
     }
     return (
         <div className="TaskItem">
             <h2 className="text-m font-bold">{props.title}</h2>
             <p>{date}</p>
-            <p> Assignee: {props.Assignee} </p>
+            <p> Assignee: {props.assigneeName} </p>
         </div>
     )
 }
