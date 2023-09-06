60e4ce7f9c339dd89ac1124928a050eb1eb75ebd
ARAVAPALLY SHIVARAMAKRISHNA <srkarvapally@gmail.com>
2023-09-06 20:23:24 +0530

    level-5 files added

--- smarter-tasks/src/App.tsx
+++ smarter-tasks/src/App.tsx
@@ -1,11 +1,86 @@
-import TaskApp from "./TaskApp";
-import './App.css';
+// import {
+//   createBrowserRouter,
+//   RouterProvider,
+// } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <TaskApp />
-    </div>
-  );
-}
+// import HomePage from './pages/HomePage';
+// import TaskListPage from './pages/TaskListPage';
+
+// import TaskApp from "./TaskApp";
+// import './App.css';
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <HomePage />,
+//   },
+//   {
+//     path: "/tasks",
+//     element: <TaskListPage />,
+//   },
+// ]);
+
+// // function App() {
+// //   return (
+// //     <div className="App">
+// //       <TaskApp />
+// //     </div>
+// //   );
+// // }
+
+// const App = () => {
+//   return (
+//     <RouterProvider router={router} />
+//   );
+// }
+
+// export default App;
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import TaskListPage from "./pages/TaskListPage";
+import TaskDetailsPage from "./pages/TaskDetailsPage";
+import Signin from "./pages/Signin";
+import ProtectedRoute from "./ProtectedRoute";
+import Layout from "./Layout";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navigate to="/signin" replace />,
+  },
+  {
+    path: "/signin",
+    element: <Signin />,
+  },
+  {
+    element: (
+      <ProtectedRoute>
+        <Layout />
+      </ProtectedRoute>
+    ),
+    children: [
+      {
+        path: "home",
+        element: <HomePage />,
+      },
+      {
+        path: "tasks",
+        element: <TaskListPage />,
+      },
+      {
+        path: "tasks/:id",
+        element: <TaskDetailsPage />,
+      },
+    ],
+  }
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+
 export default App;
--- smarter-tasks/src/Task.tsx
+++ smarter-tasks/src/Task.tsx
@@ -11,10 +11,9 @@ interface TaskItem {
 const Task = (props: TaskItem) => {
   return (
     <div className="TaskItem shadow-md border border-slate-100">
-        <h2 className="text-base font-bold my-1">{props.title}</h2>
+        <h2 className="text-base font-bold my-1">{props.title}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<button className="deleteTaskButton" id="deleteTaskButton" onClick={() => props.remove(props.id)}>[ X ]</button></h2>
         <p className="text-sm text-slate-500">DueDate: {props.dueDate}</p>
         <p className="text-sm text-slate-500">Description: {props.description}</p>
-        <button className="deleteTaskButton" id="deleteTaskButton" onClick={() => props.remove(props.id)}>Delete</button>
     </div>
   );
 };
