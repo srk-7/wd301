e138fb93badb40bbf0397a7a0ee2ab3a08d06f4d
ARAVAPALLY SHIVARAMAKRISHNA <srkarvapally@gmail.com>
2023-09-06 18:15:02 +0530

    changes told by bot

--- smarter-tasks/src/Task.tsx
+++ smarter-tasks/src/Task.tsx
@@ -14,7 +14,7 @@ const Task = (props: TaskItem) => {
         <h2 className="text-base font-bold my-1">{props.title}</h2>
         <p className="text-sm text-slate-500">DueDate: {props.dueDate}</p>
         <p className="text-sm text-slate-500">Description: {props.description}</p>
-        <button className="deleteTaskButton" onClick={() => props.remove(props.id)}>Delete</button>
+        <button className="deleteTaskButton" id="deleteTaskButton" onClick={() => props.remove(props.id)}>Delete</button>
     </div>
   );
 };
--- smarter-tasks/src/TaskForm.tsx
+++ smarter-tasks/src/TaskForm.tsx
@@ -103,7 +103,7 @@ const TaskForm = (props: TaskFormProps) => {
         </div>
         <div className="relative z-0 w-full mb-6 group">
           <button
-            type="submit"
+            type="submit" id="addTaskButton"
             className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
           >
             Add item
