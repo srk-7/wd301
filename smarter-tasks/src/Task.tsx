6acbde12f37d6ccbe4cd029a946f7160a19b3b0e
ARAVAPALLY SHIVARAMAKRISHNA <srkarvapally@gmail.com>
2023-09-09 23:27:17 +0530

    nested the anchor tag in h3 as bot rejection

--- smarter-tasks/src/Task.tsx
+++ smarter-tasks/src/Task.tsx
@@ -11,9 +11,7 @@ interface TaskItem {
 const Task = (props: TaskItem) => {
   return (
     <div className="TaskItem shadow-md border border-slate-100">
-      <a href={`/tasks/${props.id || ""}`}>
-				<h3 className="text-base font-bold my-1">{props.title}</h3>
-      </a>
+      <h3 className="text-base font-bold my-1"><a href={`/tasks/${props.id || ""}`}>{props.title}</a></h3>
       <p className="text-sm text-slate-500">DueDate: {props.dueDate}</p>
       <p className="text-sm text-slate-500">Description: {props.description}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<button className="deleteTaskButton" id="deleteTaskButton" onClick={() => props.remove(props.id)}>[ X ]</button></p>
     </div>
