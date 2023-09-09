3788943a1dd32e42a6e4607e356d98b15b6c3095
ARAVAPALLY SHIVARAMAKRISHNA <srkarvapally@gmail.com>
2023-09-09 20:30:50 +0530

    .

--- smarter-tasks/src/App.tsx
+++ smarter-tasks/src/App.tsx
@@ -69,7 +69,7 @@ const router = createBrowserRouter([
         element: <HomePage />,
       },
       {
-        path: "tasks",
+        path: "/tasks",
         element: <TaskListPage />,
       },
       {
