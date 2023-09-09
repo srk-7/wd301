750758ffe808d33be301ce0aa78eebf5502c4993
ARAVAPALLY SHIVARAMAKRISHNA <srkarvapally@gmail.com>
2023-09-09 23:17:47 +0530

    h2 to h3 in task.tsx

--- smarter-tasks/src/Task.tsx
+++ smarter-tasks/src/Task.tsx
@@ -12,7 +12,7 @@ const Task = (props: TaskItem) => {
   return (
     <div className="TaskItem shadow-md border border-slate-100">
       <a href={`/tasks/${props.id || ""}`}>
-				<h2 className="text-base font-bold my-1">{props.title}</h2>
+				<h3 className="text-base font-bold my-1">{props.title}</h3>
       </a>
       <p className="text-sm text-slate-500">DueDate: {props.dueDate}</p>
       <p className="text-sm text-slate-500">Description: {props.description}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<button className="deleteTaskButton" id="deleteTaskButton" onClick={() => props.remove(props.id)}>[ X ]</button></p>
