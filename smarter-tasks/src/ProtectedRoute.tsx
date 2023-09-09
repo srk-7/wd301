81cca1d8f788f44cdba28ab95ae5cd8039c096bf
ARAVAPALLY SHIVARAMAKRISHNA <srkarvapally@gmail.com>
2023-09-09 21:50:09 +0530

    removed main from layout

--- smarter-tasks/src/ProtectedRoute.tsx
+++ smarter-tasks/src/ProtectedRoute.tsx
@@ -2,7 +2,7 @@ import { Navigate } from "react-router-dom";
 
 export default function ProtectedRoute({ children }: { children: JSX.Element }) {
   const authenticated = localStorage.getItem("authenticated");
-  if (authenticated === 'true') {
+  if (authenticated && authenticated === 'true') {
     return <>{children}</>;
   } else {
     return <Navigate to="/signin" />;
