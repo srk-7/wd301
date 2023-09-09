838f923e62cf5403be60f95a7b2a4b852e9bd1d4
ARAVAPALLY SHIVARAMAKRISHNA <srkarvapally@gmail.com>
2023-09-10 00:00:30 +0530

    removed anchor tag and added link

--- smarter-tasks/src/Task.tsx
+++ smarter-tasks/src/Task.tsx
@@ -1,5 +1,5 @@
+import { Link } from 'react-router-dom';
 import './TaskCard.css';
-// import { TaskItem } from './types';
 interface TaskItem {
   id: number;
   title: string;
@@ -11,7 +11,9 @@ interface TaskItem {
 const Task = (props: TaskItem) => {
   return (
     <div className="TaskItem shadow-md border border-slate-100">
-      <h3 className="text-base font-bold my-1"><a href={`/tasks/${props.id || ""}`}>{props.title}</a></h3>
+      <Link to={`/tasks/${props.id}`}>
+        <h3 className="text-base font-bold my-1">{props.title}</h3>
+      </Link>
       <p className="text-sm text-slate-500">DueDate: {props.dueDate}</p>
       <p className="text-sm text-slate-500">Description: {props.description}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<button className="deleteTaskButton" id="deleteTaskButton" onClick={() => props.remove(props.id)}>[ X ]</button></p>
     </div>
