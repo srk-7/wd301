c838507b7d7a251284f7d2a6d67e121f898e2fad
ARAVAPALLY SHIVARAMAKRISHNA <srkarvapally@gmail.com>
2023-09-09 22:05:40 +0530

    added _redirects for netlify

--- smarter-tasks/src/App.tsx
+++ smarter-tasks/src/App.tsx
@@ -77,7 +77,7 @@ const router = createBrowserRouter([
         element: <TaskDetailsPage />,
       },
       {
-        path: "notfound",
+        path: "*",
         element: <Notfound />,
       },
     ],
