16e0e107a8aa12f775e07e8fac32a98b5ba851ff
ARAVAPALLY SHIVARAMAKRISHNA <srkarvapally@gmail.com>
2023-09-27 18:37:49 +0530

    some changes

--- smarter-tasks/src/pages/signin/SigninForm.tsx
+++ smarter-tasks/src/pages/signin/SigninForm.tsx
@@ -45,11 +45,11 @@ const SigninForm: React.FC = () => {
                 <input type="password" name="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} className="w-full border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue" />
             </div>
             <button type="submit" className="w-full bg-gray-700 hover:bg-gray-800 text-white font-semibold py-2 px-4 rounded-md focus:outline-none focus:shadow-outline-gray mt-4">Sign In</button>
-            {/* <center>
+            <center>
                 <button onClick={handleSignup} className="py-5 text-blue-500">
                     Don't have an Account? Sign Up
                 </button>
-           </center> */}
+           </center>
         </form>
     );
 };
