3d72bd505b4a3d1005df1fecccb5113f241ba87d
ARAVAPALLY SHIVARAMAKRISHNA <srkarvapally@gmail.com>
2023-10-10 23:24:46 +0530

    error boundary for member list and projctdetails

--- smarter-tasks/dev-dist/sw.js
+++ smarter-tasks/dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-16108a29'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.jip2483qmh8"
+    "revision": "0.lrirbti75c"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
