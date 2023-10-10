dc5830891024650ed11feb9960dbca61c2221206
ARAVAPALLY SHIVARAMAKRISHNA <srkarvapally@gmail.com>
2023-10-10 21:53:10 +0530

    .

--- smarter-tasks/src/pages/projects/ProjectList.tsx
+++ smarter-tasks/src/pages/projects/ProjectList.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-import ProjectListItems from "./ProjectListItems";
+import React from 'react';
+import ProjectListItems from './ProjectListItems';
 
 const ProjectList: React.FC = () => {
-  return (
-    <div className="suspense-loading grid gap-4 grid-cols-4 mt-5">
-      <ProjectListItems />
-    </div>
-  );
+    return (
+        <div className="suspense-loading grid gap-4 grid-cols-4 mt-5">
+            <ProjectListItems />
+        </div>
+    );
 };
 
 export default ProjectList;
